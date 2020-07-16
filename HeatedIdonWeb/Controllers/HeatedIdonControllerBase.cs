e108283591d3ca1112fe59d5b09b0223f2c6aa80
Mikko Kokkonen <mikko.kokkonen@outlook.com>
2020-07-16 20:19:20 +0300

    ASP.NET MVC default template and some implementation for MQTTNet

---
+++ HeatedIdonWeb/Controllers/HeatedIdonControllerBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HeatedIdonWeb.Helpers.Impl;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace HeatedIdonWeb.Controllers
+{
+    public class HeatedIdonControllerBase : Controller
+    {
+        protected IConfiguration _config;
+        protected MqttSubscriper _mqttSubscriper;
+        
+
+        public HeatedIdonControllerBase(IConfiguration configuration)
+        {
+            _config = configuration;
+            _mqttSubscriper = new MqttSubscriper(_config);
+        }
+    }
+}
---
+++ HeatedIdonWeb/Helpers/IMqttSubscriper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HeatedIdonWeb.Helpers.Impl;
+
+namespace HeatedIdonWeb.Helpers
+{
+    internal interface IMqttSubscriper
+    {
+
+    }
+}
